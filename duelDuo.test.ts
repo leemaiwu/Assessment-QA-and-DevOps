fa72382b96db2e2a2aade75c11afaae67f707c54
Laura Wu <leemaiwu@gmail.com>
2023-03-24 22:20:26 -0600

    Completed part 3 and 4: tests

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,8 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    driver.get('http://localhost:4000/')
+    await driver.sleep(1000)
 })
 
 afterAll(async () => {
@@ -17,4 +18,21 @@ test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
+})
+
+test('"Choose 2!" title displays after clicking "Draw" button', async () => {
+    await driver.findElement(By.xpath('//button[text()="Draw"]')).click()
+    await driver.sleep(1000)
+    const chooseTitle = await driver.findElement(By.id('choose-header'))
+    const isDisplayed = await chooseTitle.isDisplayed()
+    expect(isDisplayed).toBe(true)
+})
+
+test('"Your Duo" title displays after clicking "Add to Duo" button under a robot', async () => {
+    await driver.findElement(By.xpath('//button[text()="Draw"]')).click()
+    await driver.findElement(By.xpath('//button[text()="Add to Duo"]')).click()
+    await driver.sleep(1000)
+    const yourDuoTitle = await driver.findElement(By.id('your-duo-header'))
+    const isDisplayed = await yourDuoTitle.isDisplayed()
+    expect(isDisplayed).toBe(true)
 })
--- functions.test.ts
+++ functions.test.ts
@@ -1,5 +1,13 @@
 const {shuffleArray} = require('./utils')
 
-describe('shuffleArray should', () => {
-    // CODE HERE
+describe('shuffleArray function should ', () => {
+    test('return an array', () => {
+        expect(Array.isArray(shuffleArray(['']))).toBe(true)
+    })
+    
+    test('return an array with the same number of inputs', () => {
+        const array = [1, 2, 3, 4]
+        const shuffleArrayTest = shuffleArray(array)
+        expect(shuffleArrayTest).toHaveLength(array.length)
+  })
 })
